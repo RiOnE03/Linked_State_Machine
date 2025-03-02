82a7f81038a00e503d1a6a4a37f551d67244f64a
RiOnE03 <130093862+RiOnE03@users.noreply.github.com>
2025-03-02 19:48:13 +0530

    Updated Comments

--- Main_Scripts/Linked_State.gd
+++ Main_Scripts/Linked_State.gd
@@ -1,23 +1,30 @@
 @tool
+## The state node which will be the main node that has the proproties needed for a state Machine such as Enter, Exist, State_Initiate, State_Input etc.
 class_name Linked_State extends Linked_State_Root
 
+## The states that will start if the current state called for transition
 @export var next_states:Array[State_carrior]:
 	set(value):
 		next_states = value
 		if !next_states.is_empty() and !is_instance_valid(next_states[next_states.size()-1]):
 			next_states[next_states.size()-1] = State_carrior.new()
 
+## when this property is true. This state node will act as the Linked State root node however while 
+## also still behaving as a state node running the important functions such as Enter, Exist, Update, Physics_Update etc.
 @export var is_always_active: bool = false:
 	set(value):
 		is_always_active = value
 		notify_property_list_changed()
 
+## defines the current control type
 var control_state: State_carrior.control_type
 
-
+## The active linked states that are running only stores for one level of linked nodes 
 var active_states: Array[State_carrior]
 
+## Checks if the state is currently active
 var is_active: bool = false
+## The node that currently it is linked to. Only one at a time.
 var caller: Linked_State_Root
 
 
@@ -48,7 +55,7 @@ func _input(event: InputEvent) -> void:
 	if is_active:
 		state_input(event)
 
-
+## Starts all the states given in the form of state carrier's Array
 func activate_states(states: Array[State_carrior])->void:
 	for state_carrior in states:
 		if is_instance_valid(state_carrior.Ref):
@@ -68,8 +75,11 @@ func _ready() -> void:
 			start_state(self,control_state)
 		state_initiate()
 
+## The function that is equivalent of ready for states
+func state_initiate()->void:
+	pass
 
-
+## Internal function equivalent of Enter and handles the inbuilt functionality.
 func start_state(caller_Ref: Linked_State_Root, Mode: State_carrior.control_type)->void:
 	if caller == caller_Ref or is_always_active: return
 	caller = caller_Ref
@@ -98,7 +108,7 @@ func start_state(caller_Ref: Linked_State_Root, Mode: State_carrior.control_type
 			active_states.clear()
 	Enter()
 
-
+## Internal function equivalent of Exit and handles the inbuilt functionality.
 func end_state(caller_Ref: Linked_State_Root)->void:
 	if !is_active or is_always_active or caller!=caller_Ref:
 		return
@@ -107,26 +117,28 @@ func end_state(caller_Ref: Linked_State_Root)->void:
 	deactive_states(active_states)
 	Exit()
 
+## Called when the state activates for the first time and won't called again until Exit is called
 func Enter()->void:
 	pass
 
+## Called when the state deactivates. Should not be called directly, only define the logic for it. It will be called interanlly.
 func Exit()->void:
 	pass
 
-
+## Equivalent of _process for states
 func Update(_delta: float)->void:
 	pass
-
+## Equivalent of _physics_process for states
 func Physics_Update(_delta:float)->void:
 	pass
-
+## Equivalent of _input for states
 func state_input(_event: InputEvent)->void:
 	pass
-
+## Call this function when want to go to next states. Calling this will stop the current state.
 func transition_to_next_states()->void:
 	if is_active and is_instance_valid(caller):
 		caller.process_states(self)
-
+## The internal function used for transitioning the linked states to their next states
 func process_states(sender_state:Linked_State):
 	var carrier: State_carrior
 	for child in active_states:
--- Main_Scripts/Linked_State_Root.gd
+++ Main_Scripts/Linked_State_Root.gd
@@ -1,13 +1,18 @@
 @tool
+## The base class for Linked State Machine and the root of all the states. This class only acts as the last point for the propogation of transition signal and do not process anything. It will directly transition the states to their next states.
+## Some of the properties also do not work with this class such as FULL_LINK_CONTINUE and FULL_LINK_RESET works the same since they only work for the time of initiation and then the 
+## process of the state activations depends on the passing states themselves. States themselves will then be responsible for deciding the next states situation. 
 class_name Linked_State_Root extends Node
 
 
+## The linked states that will start along with the current states if the control type is not Isolated for the current state.
 @export var linked_states: Array[State_carrior]:
 	set(value):
 		linked_states = value
 		if !linked_states.is_empty() and !is_instance_valid(linked_states[linked_states.size()-1]):
 			linked_states[linked_states.size()-1] = State_carrior.new()
 
+## State_carriers are not perfect and needs to be setup before using them. Make sure to set them up in ready or state_initiate using this method.
 func setup_state_carriers(carriars: Array[State_carrior])->Array[State_carrior]:
 	if not Engine.is_editor_hint():
 		for carriar in carriars:
@@ -21,22 +26,21 @@ func _ready():
 	if not Engine.is_editor_hint():
 		setup_state_carriers(linked_states)
 		activate_states(linked_states)
-		state_initiate()
 
-func state_initiate()->void:
-	pass
 
+## Starts all the states given in the form of state carrier's Array
 func activate_states(states: Array[State_carrior])->void:
 	for state_carrior in states:
 		if is_instance_valid(state_carrior.Ref):
 			state_carrior.Ref.start_state(self,state_carrior.control)
 
+## Stops all the states given in the form of state carrier's Array
 func deactive_states(states: Array[State_carrior])->void:
 	for state_carrior in states:
 		if is_instance_valid(state_carrior.Ref):
 			state_carrior.Ref.end_state(self)
 
-
+## The internal function used for transitioning the linked states to their next states
 func process_states(sender_state:Linked_State):
 	for child in linked_states:
 		if child.Ref==sender_state:
